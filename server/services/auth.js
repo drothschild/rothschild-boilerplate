6ebe036450080cbb843317259268419ab5ffc838
drothschild <david@rothschildsolutions.com>
2017-10-17 17:23:57 -0700

    Split out createToken into its own function

--- server/services/auth.js
+++ server/services/auth.js
@@ -42,13 +42,15 @@ async function signup({ email, password, req }) {
     throw new Error('You must provide an email and password.');
   }
   const user = await new User({ email, password });
-  let token, host, tokenURL;
+  const host = req.headers['x-forwarded-host']
+    ? req.headers['x-forwarded-host']
+    : req.headers['host'];
   const existingUser = await User.findOne({ email });
   if (existingUser) {
     throw new Error('Email already registered.');
   }
   await user.save();
-  await createLoginToken(user);
+  createToken(user, host);
   return new Promise((resolve, reject) => {
     req.logIn(user, err => {
       if (err) {
@@ -59,22 +61,48 @@ async function signup({ email, password, req }) {
   });
 }
 
-async function createLoginToken(user) {
+async function createToken(user, host) {
   const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET, {
     expiresIn: '12h',
   });
-
-  const host = req.headers['x-forwarded-host']
-    ? req.headers['x-forwarded-host']
-    : req.headers['host'];
   const tokenURL = `http://${host}/token/${token}`;
-  // Normally I'd leave console log out- but just in case the mail fails..
+  // Normally I'd leave console log out- but just in case the mail fails
   console.log('Magic Token', tokenURL);
   await mail.send({
     user,
     filename: 'new-account',
     subject: 'New User Account',
     tokenURL,
+  });
+}
+
+async function loginWithLink({ token, req }) {
+  let decodedToken, id;
+  try {
+    decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+  } catch (err) {
+    throw new Error('Invalid Token');
+  }
+  id = decodedToken.id;
+  const user = await User.findById(id);
+  return new Promise((resolve, reject) => {
+    if (!user) {
+      reject('Invalid credentials.');
+    }
+
+    req.login(user, () => resolve(user));
+  });
+}
+
+function login({ email, password, req }) {
+  return new Promise((resolve, reject) => {
+    passport.authenticate('local', (err, user) => {
+      if (!user) {
+        reject('Invalid credentials.');
+      }
+
+      req.login(user, () => resolve(user));
+    })({ body: { email, password } });
   });
 }
 
